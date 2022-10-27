ef116cf35892f5659d07a7c3a7acf8cee39d74a5
kawinnat09 <Kwinnathmayura@gmail.com>
2022-10-27 16:28:18 +0700

    update

--- main.js
+++ main.js
@@ -7,6 +7,14 @@ const app = Vue.createApp({
             url:'https://www.apple.com',
             inStock:true,
             inventory:0,
+            details: ["64GB","128GB","256GB"],
+            varians: [
+                {
+                    id: 239, color: "Purple", image:"./assets/images/iphone-12-purple.png",
+                    id: 240, color: "Red", image:"./assets/images/iphone-12-red.png",
+                    id: 241, color: "White", image:"./assets/images/iphone-12-white.png",
+                }
+            ]
         }
     }
 }).mount("#app")
